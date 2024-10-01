9464f036cbeff977c565ec571ec5b1fe0fea17de
Oliver Carlson <olivercarlson@Olivers-MacBook-Pro.local>
2024-09-30 18:19:26 -0700

    add 3 longest substring without repeats

---
+++ 3-longest_substring_without_repeating_characters.py
@@ -0,0 +1,15 @@
+class Solution:
+    # SOLUTION:
+    # TC: O(N) iteration through s
+    # SC: O(1) since maximum size of seen is: alphabet (upper + lower) + spaces + symbols = 52 + space + symbols ~= 65?
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        max_ss = 0
+        seen = {}
+        for r in range(len(s)):
+            if s[r] in seen:
+                l = max(l,seen[s[r]] + 1)
+            cur = s[l:r+1]
+            max_ss = max(len(cur), max_ss)
+            seen[s[r]] = r
+        return max_ss
