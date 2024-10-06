3c1ca31aa51787cbd2df2b1a12afccbf7bea6514
Oliver Carlson <olivercarlson@Olivers-MacBook-Pro.local>
2024-10-06 15:13:06 -0700

    add clone graph +d fs

--- 133-clone_graph.py
+++ 133-clone_graph.py
@@ -9,8 +9,8 @@ class Node:
 from typing import Optional
 class Solution:
     # SOLUTION #1: BFS
-    # TC: O(N) -- must process each node at least once
-    # SC: O(N) -- visited object is duplicate of graph.
+    # TC: O(N+M) -- must process each vertex + edge at least once
+    # SC: O(N) -- visited is a store of all vertices
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return
@@ -33,5 +33,26 @@ class Solution:
                     output[n] = Node(n)
                 output[val].neighbors.append(output[n])
         return output[1]
+    # SOLUTION #2: DFS
+    # TC: O(N+M) -- must process each vertex and each edge at least once.
+    # SC: O(N) -- visited object is duplicate of all nodes (and call stack is size of O(N) too)
+    def dfs(self, node, copy, visited = {}):
+        if copy.val in visited:
+            return
+        visited[copy.val] = copy
+        for n in node.neighbors:
+            if n.val not in visited:
+                new_node = Node(n.val)
+                copy.neighbors.append(new_node)
+                self.dfs(n, new_node, visited)
+            else:
+                copy.neighbors.append(visited[n.val])
+        return node
 
-
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        copy = Node(1)
+        visited = {}
+        self.dfs(node, copy, visited)
+        return copy
